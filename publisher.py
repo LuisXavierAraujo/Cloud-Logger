70f527b01d362a46bf11fe6c983772a90b8c6ae7
Luís Araújo <91569870+LuisXavierAraujo@users.noreply.github.com>
2022-11-25 18:12:57 +0000

    Add files via upload

---
+++ publisher.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 25 16:56:58 2022
+
+@author: luise
+"""
+
+import paho.mqtt.client as mqtt
+
+
+
+client = mqtt.Client()
+#client.on_connect = on_connect
+client.connect("testmosquito.org", 1883, 60)
+client.publish("luisaraujo", "on")
