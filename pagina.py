51180834c0552a74cc8f33ec2c1ab367750a2cb9
Luís Araújo <91569870+LuisXavierAraujo@users.noreply.github.com>
2022-11-28 11:55:54 +0000

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -10,7 +10,7 @@ import pandas as pd
 import streamlit as st
 import paho.mqtt.client as mqtt
 import threading as th
-from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx
+from streamlit.runtime.scriptrunner.script_run_context import add_script_run_ctx
 
 
 #MQTT Thread Function
