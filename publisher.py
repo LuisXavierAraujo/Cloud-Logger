cb275459958e1da7afe59d1a1d5133182acd6d11
Luís Araújo <91569870+LuisXavierAraujo@users.noreply.github.com>
2022-12-02 15:09:54 +0000

    Delete publisher.py

--- publisher.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client()
-#client.on_connect = on_connect
-client.connect("mqtt.eclipseprojects.io", 1883, 60)
-a = [[0.005,0.003,0.002],[2,3,1]]
-a_bytearray = str(a)
-client.publish("luisaraujo", a_bytearray)
